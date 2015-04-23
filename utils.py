8ac4b2e2e7bc6754140e328ecf0f8c554ca493c3
Surojit Pathak <suro@yahoo-inc.com>
2015-04-23 23:22:25 +0000

    Fix the end index

--- utils.py
+++ utils.py
@@ -5,5 +5,5 @@ import rlcompleter
 readline.parse_and_bind('tab:complete')
 
 def suro_util_history():
-    for i in range(1, readline.get_current_history_length() + 1):
+    for i in range(1, readline.get_current_history_length()):
         print readline.get_history_item(i)
