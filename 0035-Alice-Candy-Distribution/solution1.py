f637b8b115fa69dddd5bc754a1a2d894209b16b9
Surojit Pathak <suro@yahoo-inc.com>
2016-03-03 10:36:02 -0800

    Linear time solution.

--- 0035-Alice-Candy-Distribution/solution1.py
+++ 0035-Alice-Candy-Distribution/solution1.py
@@ -1,14 +1,22 @@
 #!/bin/python
 import sys
 
+def fix_left_subarr(ranks, candies, k):
+    for i in range(k, 0, -1):
+        if ranks[i-1] > ranks[i] and candies[i-1] <= candies[i]:
+           candies[i-1] = candies[i-1] + 1
+        else:
+           break
+    
 def find_minimum_candies_required(ranks, n):
     candies = [ 1 for i in range(0,n) ]
     for i in range(1, n):
-        if ranks[i-1] < ranks[i] and candies[i-1] >= candies[i]:
+        if ranks[i-1] > ranks[i]:
+            fix_left_subarr(ranks, candies, i)
+        elif ranks[i-1] < ranks[i]:
             candies[i] = candies[i-1] + 1
-    for i in range(n-2, -1, -1):
-        if ranks[i] > ranks[i+1] and candies[i] <= candies[i+1]:
-            candies[i] = candies[i+1] + 1
+        else:
+            candies[i] = 1
     sum = 0
     for i in candies:
        sum = sum + i    
